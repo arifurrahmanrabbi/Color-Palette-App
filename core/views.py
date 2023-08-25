12bc88e4bbf0ef87a4179a181b98e70c466330d6
Arifur Rahman Rabbi <marifurrahmanrabbi@gmail.com>
2023-08-25 12:01:42 +0600

    Implement login and logout

--- core/views.py
+++ core/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        #If user is logged in show home page
+        return render(request, 'home.html')
+    #Else redirect to login page
+    return redirect('login')
+
+def login_user(request):
+    #If user is trying to log in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+        else:
+            #If user is not found
+            messages.success(request, "Error")
+        #Redirect to Home page
+        return redirect('home')
+    #Else show the login page
+    return render(request, 'login.html')
+
+def logout_user(request):
+    #Logout user
+    logout(request)
+    messages.success(request, "Logged Out")
+    #Redirect to login page
+    return redirect('login')
